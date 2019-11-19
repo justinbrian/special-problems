5c9f94d8cb60e0931d1376bd502cdd9872411735
JB <justinbrian@users.noreply.github.com>
2019-11-18 18:22:04 -0800

    Update movie.php

--- movie.php
+++ movie.php
@@ -1,79 +1,121 @@
 <!doctype html>
+
+
+<?php
+// Get current movie title int the html title.
+$page = isset($_GET['menu'])?$_GET['menu']:'movie';
+$page = isset($_GET['menu'])?$_GET['menu']:'movie1';
+$page = isset($_GET['menu'])?$_GET['menu']:'movie2';
+$page = isset($_GET['menu'])?$_GET['menu']:'movie3';
+
+switch($page){
+	case 'movie':
+		$movie = $_GET["film"];
+		$title = 'Mortal Kombat';
+        $content = 'movie.php?film=mortalkombat';
+		break;
+	case 'movie1':
+		$movie = $_GET["film"];
+		$title = 'Princess Bride';
+		$content = 'movie.php?film=princessbride';
+		break;
+	case 'movie2':
+		$movie = $_GET["film"];
+		$title = 'Teenage Mutant Ninja Turtles';
+		$content = 'movie.php?film=tmnt';
+		break;
+	case 'movie3':
+		$movie = $_GET["film"];
+		$title = 'Teenage Mutant Ninja Turtles 2';
+		$content = 'movie.php?film=tmnt2';
+		break;
+
+}
+// The following html could be in a separate file and included, eg layout.php.
+?>
+
 <html>
 	
 	<head>
-		<title>Rancid Tomatoes</title>
-
+		<title>Rancid Tomatoes - <?php echo $title;?></title>
 		<meta charset="utf-8" />
 		<link href="css/movie.css" type="text/css" rel="stylesheet" />
 		<link rel="icon" type="image/gif" href="img/movie/rotten.gif" />
-		
-		<!-- 
-		Stephen Brannen
-		CSC 365 Fall 2013
-		Missouri State University
-		Homework 3
-		tmnt.html
-		Eccentric: \\eccentric\class\csc365\001\brannen01\HW3
-		Personal Site: http://csc.stephenbrannen.com/HW3/movie.php
-		-->
-		
 	</head>
 	
 	<?php
-		//variables
+		// Initialize vars.
+		
+		// Get the film name.
 		$movie = $_GET["film"];
+
+		// Get the movie info.
 		$raw_info = file_get_contents("$movie/info.txt");
+
+		// Movie overview.
 		$raw_overview = file_get_contents("$movie/overview.txt");
+
+		// Info.
 		$info = explode("\n", $raw_info);
+
+		// Overview.
 		$overview = explode("\n", $raw_overview);
+
+		// Count of reviews.
 		$count = 0;
+
+		// Freshness.
 		$freshness = "";
 		$freshAlt = "";
-		
-		//this sets the value for freshness and freshAlt
-		if (intval($info[2]) >= 60){
+	
+		if (intval($info[2]) >= 60)
+		{
 				$freshness = "img/movie/freshbig.png";
 				$freshAlt = "Fresh";
 			}
-			else{
+			else
+			{
 				$freshness = "img/movie/rottenbig.png";
 				$freshAlt = "Rotten";
 			}
 		
-		//functions
-		
-		//this function displays the overview sidebar 
-		function sidebar(){
+		// Display overview slider.
+		function sidebar()
+		{
 			global $overview;
-			foreach($overview as $row){
+			foreach($overview as $row)
+			{
 				$row = explode(":", $row);
 				echo "<dt>{$row[0]}</dt><dd>{$row[1]}</dd>";
 			}
 		}
 		
-		//this function gets the review information
-		function getReviews(){
+		// Get review information.
+		function getReviews()
+		{
 			global $movie;
 			global $count;
 			$raw_review = array();
-			foreach (glob("$movie/review*.txt") as $filename){
+			foreach (glob("$movie/review*.txt") as $filename)
+			{
 				$raw_review[$count] = file_get_contents("$filename");
 				$count++;
 			}
-			for ($i=0;$i<$count;$i++){
+			for ($i=0;$i<$count;$i++)
+			{
 				$review = explode("\n", $raw_review[$i]);
 				displayReview($review,$i);
 			}
 		}
 		
-		//this function displays the review information
-		function displayReview($review,$num){
+		// Display review information.
+		function displayReview($review,$num)
+		{
 			global $count;
 			$num++;
 			$review[1] = strtolower($review[1]);
 			echo "<p class='review'>
-						<img src='{$review[1]}.gif' alt='{$review[1]}' />
+						<img src='img/movie/{$review[1]}.gif' alt='{$review[1]}' />
 						<q>{$review[0]}</q>
 				 </p>
 				 <p class='reviewer'>
@@ -95,9 +137,9 @@
 
 		<h1 id="title"><?echo "$info[0] ($info[1])";?></h1>
 		
-		<div id="overall"> <!-- Start of main section -->
-		
-			<div id="right"> <!-- Start of right section -->
+		<div id="overall"> <!-- Main section. -->
+
+			<div id="right"> <!-- Start of right section. -->
 				<div>
 					<img id="overview_img" src="<?=$movie?>/overview.png" alt="general overview" />
 				</div>
@@ -106,29 +148,32 @@
 					<?php sidebar();?>
 				</dl>
 
-			</div> <!-- End of right section -->
+			</div> <!-- End of right section. -->
 
-			<div id="left"> <!-- Start of left section -->
+			<div id="left"> <!-- Start of left section. -->
 
 				<div id="score">
 					<img id="rotten" src="<?=$freshness?>" alt="<?=$freshAlt?>" />
 					<?=$info[2]?>%
 				</div>
 				
-				<div class="column"> <!-- Start of Reviews -->
+				<div class="column"> <!-- Start of reviews. -->
 					<?php getReviews();?>				
 				</div> <!-- End of reviews -->
+
 				<div id="aftercolumns"></div>
 
-			</div> <!-- End of left section -->
+			</div> <!-- End of left section. -->
 
 				<p id="footer">(1- <?=$count?>) of <?=$count?></p>
-		</div> <!-- End of main section -->
+		</div> <!-- End of main section. -->
 		
 		<div id="validate">
-						<p><a href="https://webster.cs.washington.edu/validate-html.php"><img src="img/movie/w3c-html.png" alt="HTML Validator" /></a><br />
-<a href="https://webster.cs.washington.edu/validate-css.php"><img src="img/movie/w3c-css.png" alt="CSS Validator" /></a></p>
+			<p><a href="https://webster.cs.washington.edu/validate-html.php">
+			<img src="img/movie/w3c-html.png" alt="HTML Validator" /></a><br />
 
+			<a href="https://webster.cs.washington.edu/validate-css.php">
+			<img src="img/movie/w3c-css.png" alt="CSS Validator" /></a></p>
 		</div>
 	</body>
 </html>
